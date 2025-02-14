e4b76b272a9c247bdd6fa384d0f0c8e4707b0751
HydraTal <jacobtpeters12@gmail.com>
2025-02-13 17:21:52 -0700

    Entity Variants

--- src/main/java/net/kaupenjoe/tutorialmod/entity/custom/GeckoEntity.java
+++ src/main/java/net/kaupenjoe/tutorialmod/entity/custom/GeckoEntity.java
@@ -1,11 +1,23 @@
 package net.kaupenjoe.tutorialmod.entity.custom;
 
+import net.kaupenjoe.tutorialmod.entity.GeckoVariant;
 import net.kaupenjoe.tutorialmod.entity.ModEntities;
 import net.kaupenjoe.tutorialmod.item.ModItems;
+import net.minecraft.Util;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.AnimationState;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.BossEvent;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -13,11 +25,18 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import org.jetbrains.annotations.Nullable;
 
 public class GeckoEntity extends Animal {
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
+
+    private static final EntityDataAccessor<Integer> VARIANT =
+            SynchedEntityData.defineId(GeckoEntity.class, EntityDataSerializers.INT);
+
+//    private final ServerBossEvent bossEvent =
+//            new ServerBossEvent(Component.literal("Our Mighty Gecko"), BossEvent.BossBarColor.GREEN, BossEvent.BossBarOverlay.NOTCHED_10);
 
     public GeckoEntity(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
@@ -53,7 +72,10 @@ public class GeckoEntity extends Animal {
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob otherParent) {
-        return ModEntities.GECKO.get().create(level);
+        GeckoVariant variant = Util.getRandom(GeckoVariant.values(), this.random);
+        GeckoEntity baby = ModEntities.GECKO.get().create(level);
+        baby.setVariant(variant);
+        return baby;
     }
 
     private void setupAnimationStates() {
@@ -73,4 +95,82 @@ public class GeckoEntity extends Animal {
             this.setupAnimationStates();
         }
     }
+
+    /* VARIANT */
+    @Override
+    protected void defineSynchedData(SynchedEntityData.Builder builder) {
+        super.defineSynchedData(builder);
+        builder.define(VARIANT, 0);
+    }
+
+    private int getTypeVariant() {
+        return this.entityData.get(VARIANT);
+    }
+
+    public GeckoVariant getVariant() {
+        return GeckoVariant.byId(this.getTypeVariant() & 255);
+    }
+
+    private void setVariant(GeckoVariant variant) {
+        this.entityData.set(VARIANT, variant.getId() & 255);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag compound) {
+        super.addAdditionalSaveData(compound);
+        compound.putInt("Variant", this.getTypeVariant());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag compound) {
+        super.readAdditionalSaveData(compound);
+        this.entityData.set(VARIANT, compound.getInt("Variant"));
+    }
+
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty,
+                                        MobSpawnType spawnType, @Nullable SpawnGroupData spawnGroupData) {
+        GeckoVariant variant = Util.getRandom(GeckoVariant.values(), this.random);
+        this.setVariant(variant);
+        return super.finalizeSpawn(level, difficulty, spawnType, spawnGroupData);
+    }
+
+    /* SOUNDS */
+
+    @Nullable
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.PANDA_AMBIENT;
+    }
+
+    @Nullable
+    @Override
+    protected SoundEvent getHurtSound(DamageSource damageSource) {
+        return SoundEvents.FROG_HURT;
+    }
+
+    @Nullable
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.BAT_DEATH;
+    }
+
+//    /* BOSS BAR */
+//    @Override
+//    public void startSeenByPlayer(ServerPlayer serverPlayer) {
+//        super.startSeenByPlayer(serverPlayer);
+//        this.bossEvent.addPlayer(serverPlayer);
+//    }
+//
+//    @Override
+//    public void stopSeenByPlayer(ServerPlayer serverPlayer) {
+//        super.stopSeenByPlayer(serverPlayer);
+//        this.bossEvent.removePlayer(serverPlayer);
+//    }
+//
+//    @Override
+//    public void aiStep() {
+//        super.aiStep();
+//        this.bossEvent.setProgress(this.getHealth() / this.getMaxHealth());
+//    }
 }
